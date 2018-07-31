b7d55fb7fbdc6e143f21eaba1eee26598ff247b7
myatejx <myatejx@126.com>
2018-07-31 17:29:33 +0800

    viewgroup变形容纳

--- app/src/main/java/com/myatejx/animatetest/layout_trans/TestLayoutChangeActivity.java
+++ app/src/main/java/com/myatejx/animatetest/layout_trans/TestLayoutChangeActivity.java
@@ -1,8 +1,13 @@
 package com.myatejx.animatetest.layout_trans;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.myatejx.animatetest.R;
+import com.myatejx.animatetest.databinding.ActivityLayoutTransBinding;
 
 /**
  * @author xmj
@@ -10,11 +15,19 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class TestLayoutChangeActivity extends AppCompatActivity {
 
-
+    private ActivityLayoutTransBinding mBinding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_layout_trans);
+        mBinding.setClickProxy(new ClickProxy());
+    }
 
+    public class ClickProxy {
+        public void expand() {
+            mBinding.calendar.setVisibility(
+                    mBinding.calendar.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+        }
     }
 }
