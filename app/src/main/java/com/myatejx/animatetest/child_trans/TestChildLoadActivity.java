7e49ffb5a2fbd434dd5812e1c96a47e4f1b5d5f6
myatejx <myatejx@126.com>
2018-07-31 17:09:12 +0800

    chide动画载入

--- app/src/main/java/com/myatejx/animatetest/child_trans/TestChildLoadActivity.java
+++ app/src/main/java/com/myatejx/animatetest/child_trans/TestChildLoadActivity.java
@@ -1,8 +1,17 @@
 package com.myatejx.animatetest.child_trans;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.myatejx.animatetest.R;
+import com.myatejx.animatetest.databinding.ActivityChildTransBinding;
+import com.myatejx.animatetest.databinding.AdapterChildTransBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * @author xmj
@@ -10,11 +19,39 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class TestChildLoadActivity extends AppCompatActivity {
 
-
+    private ActivityChildTransBinding mBinding;
+    private BaseBindingAdapter<String, AdapterChildTransBinding> mAdapter;
+    private List<String> mStrings = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_child_trans);
+        mBinding.setClickProxy(new ClickProxy());
+        mAdapter = new BaseBindingAdapter<String, AdapterChildTransBinding>(getApplicationContext()) {
+            @Override
+            protected int getLayoutResId(int viewType) {
+                return R.layout.adapter_child_trans;
+            }
 
+            @Override
+            protected void onBindItem(AdapterChildTransBinding binding, String item, int position) {
+
+            }
+        };
+        mBinding.rv.setAdapter(mAdapter);
+        for (int i = 0; i < 20; i++) {
+            mStrings.add("0");
+        }
+        mAdapter.setList(mStrings);
+        mAdapter.notifyDataSetChanged();
+    }
+
+    public class ClickProxy {
+        public void reload() {
+            //TODO 没效果？
+            mAdapter.setList(mStrings);
+            mAdapter.notifyDataSetChanged();
+        }
     }
 }
